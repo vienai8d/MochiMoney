a6a8fbc12f93674ef95477b615a45f8944d6a403
vienai8d <31370233+vienai8d@users.noreply.github.com>
2025-03-31 09:56:13 +0900

    update app/main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 
 def main():
+    st.set_page_config(page_title="TOP", page_icon="./assets/icon/mochi_icon_512.png", layout="centered")
     st.title("MochiMoney")
+    st.image("./assets/icon/mochi_icon_512.png")
 
 
 if __name__ == "__main__":
