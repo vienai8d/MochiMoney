da6f8cccf1d99aa648e5c93842409e4f30f3b8d8
vienai8d <31370233+vienai8d@users.noreply.github.com>
2025-04-05 22:27:40 +0900

    update app/Home.py

--- app/Home.py
+++ app/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def main():
-    st.set_page_config(page_title="TOP", page_icon="./assets/icon/mochi_icon_512.png", layout="centered")
+    st.set_page_config(page_title="Home", page_icon="./assets/icon/mochi_icon_512.png", layout="centered")
     st.title("MochiMoney")
     st.image("./assets/icon/mochi_icon_512.png")
 
